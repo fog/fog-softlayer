b57f078ec79f6be4e7860f7c84f5a70adf7bf56c
Matt Eldridge <matt.eldridge@us.ibm.com>
2014-05-29 12:48:47 -0400

    Remove uneeded multi provider logic from test helper.

--- tests/helper.rb
+++ tests/helper.rb
@@ -37,32 +37,3 @@ end
 def array_differences(array_a, array_b)
   (array_a - array_b) | (array_b - array_a)
 end
-
-# check to see which credentials are available and add others to the skipped tags list
-all_providers = Fog.registered_providers.map {|provider| provider.downcase}
-
-# Manually remove these providers since they are local applications, not lacking credentials
-all_providers = all_providers - ["libvirt", "vmfusion", "openvz"]
-
-available_providers = Fog.available_providers.map {|provider| provider.downcase}
-
-unavailable_providers = all_providers - available_providers
-
-if !ENV['PROVIDER'].nil? && unavailable_providers.include?(ENV['PROVIDER'])
-  Formatador.display_line("[red]Requested provider #{ENV['PROVIDER']} is not available.[/]" + 
-                          "[red]Check if .fog file has correct configuration (see '#{Fog.credentials_path}')[/]")
-  exit(0)
-end
-
-for provider in unavailable_providers
-  Formatador.display_line("[yellow]Skipping tests for [bold]#{provider}[/] [yellow]due to lacking credentials (add some to '#{Fog.credentials_path}' to run them)[/]")
-  Thread.current[:tags] << ('-' << provider)
-end
-
-# mark libvirt tests pending if not setup
-begin
-  require('libvirt')
-rescue LoadError
-  Formatador.display_line("[yellow]Skipping tests for [bold]libvirt[/] [yellow]due to missing `ruby-libvirt` gem.[/]")
-  Thread.current[:tags] << '-libvirt'
-end
