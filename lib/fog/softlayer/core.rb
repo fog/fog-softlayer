e4727f0077e21d23c6f491bc495e6060842c67d5
Akira Matsuda <ronnie@dio.jp>
2014-09-04 16:38:11 +0900

    Avoid overwriting Hash#deep_merge
    Bump version.

--- lib/fog/softlayer/core.rb
+++ lib/fog/softlayer/core.rb
@@ -135,10 +135,12 @@ module Fog
 
 end
 
-class Hash
-  def deep_merge(second)
-    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
-    self.merge(second, &merger)
+unless {}.respond_to? :deep_merge
+  class Hash
+    def deep_merge(second)
+      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+      self.merge(second, &merger)
+    end
   end
 end
 
--- lib/fog/softlayer/version.rb
+++ lib/fog/softlayer/version.rb
@@ -7,6 +7,6 @@
 
 module Fog
   module Softlayer
-    VERSION = "0.3.16"
+    VERSION = "0.3.17"
   end
 end
